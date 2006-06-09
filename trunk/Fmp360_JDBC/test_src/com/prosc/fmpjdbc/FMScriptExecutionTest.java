0057291eb12bee4cdfb3d890f6ddbfbed10a995f
britty654 <britty654@5e039c8d-0506-18fe-fdc3-72db6750e6f2>
2006-06-09 15:59:07 +0000

    multi-threading so that we can _stream_ back results from FM -britt

--- trunk/Fmp360_JDBC/test_src/com/prosc/fmpjdbc/FMScriptExecutionTest.java
+++ trunk/Fmp360_JDBC/test_src/com/prosc/fmpjdbc/FMScriptExecutionTest.java
@@ -7,48 +7,51 @@ import java.net.URL;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.Iterator;
 
 /**
  * Created by IntelliJ IDEA. User: brian Date: Apr 20, 2005 Time: 4:57:57 PM To change this template use File | Settings
  * | File Templates.
  */
 public class FMScriptExecutionTest extends TestCase {
-	private FmConnection connection;
-	private FmXmlRequest request;
-	private static final String username = null;
-	private static final String password = null;
-	private static final String host = "orion.360works.com:80";
-	private static final String driverClassName = "com.prosc.fmpjdbc.Driver";
-	private String jdbcUrl;
-	private FmStatement statement;
+  private FmConnection connection;
+  private FmXmlRequest request;
+  private static final String username = null;
+  private static final String password = null;
+  private static final String host = "orion.360works.com:80";
+  private static final String driverClassName = "com.prosc.fmpjdbc.Driver";
+  private String jdbcUrl;
+  private FmStatement statement;
 
-	protected void setUp() throws Exception {
-		Class.forName( driverClassName );
-		jdbcUrl = "jdbc:fmp360://" + host + "/Contacts";
-		connection = (FmConnection) DriverManager.getConnection( jdbcUrl, username, password );
-		Logger.getLogger("com.prosc.fmpjdbc").setLevel(Level.ALL);
-		statement = (FmStatement) connection.createStatement();
-	}
+  protected void setUp() throws Exception {
+    Class.forName( driverClassName );
+    jdbcUrl = "jdbc:fmp360://" + host + "/Contacts";
+    connection = (FmConnection) DriverManager.getConnection( jdbcUrl, username, password );
+    Logger.getLogger("com.prosc.fmpjdbc").setLevel(Level.ALL);
+    statement = (FmStatement) connection.createStatement();
+  }
 
-	protected void tearDown() throws Exception {
-		statement.close();
-		connection.close();
-	}
+  protected void tearDown() throws Exception {
+    statement.close();
+    connection.close();
+  }
 
-	public void testFMScriptExecution() throws IOException, FileMakerException {
-		//-db=employees&-lay=departments&-script=myscript&-findany
-		if ( new JDBCTestUtils().use360driver ) {
-			try {
-				request = connection.getXmlRequestHandler();
-				try {
-					request.doRequest("-db=Contacts&-lay=Contacts&-max=0&-script=Create New Person&-findall");
-				} finally {
-					request.closeRequest();
-				}
-			} catch (IOException ioe) {
-				throw ioe;
-			}
-		}
-	}
+  public void testFMScriptExecution() throws IOException, FileMakerException {
+    //-db=employees&-lay=departments&-script=myscript&-findany
+    if ( new JDBCTestUtils().use360driver ) {
+      try {
+        request = new FmXmlRequest(connection.getProtocol(), connection.getHost(), connection.getFMVersionUrl(),
+            connection.getPort(), connection.getUsername(), connection.getPassword(), connection.getFmVersion());
+        try {
+          request.doRequest("-db=Contacts&-lay=Contacts&-max=0&-script=Create New Person&-findall");
+
+        } finally {
+          request.closeRequest();
+        }
+      } catch (IOException ioe) {
+        throw ioe;
+      }
+    }
+  }
 
 }
