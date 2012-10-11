1cac44c4af84babc9ded766f4f4465208e9c37bc
jbarnum <jbarnum@5e039c8d-0506-18fe-fdc3-72db6750e6f2>
2012-10-11 16:45:37 +0000

    Incremented version to 1.3134
    Now includes more user-friendly error messages when validation fails while writing to client
    
    Moved error code message lookups to public method in ErrorCodes class, so anybody can look up FileMaker error messages

--- trunk/Fmp360_JDBC/src/com/prosc/sql/ErrorCodes.java
+++ trunk/Fmp360_JDBC/src/com/prosc/sql/ErrorCodes.java
@@ -1,10 +1,44 @@
 package com.prosc.sql;
 
+import com.prosc.fmpjdbc.FileMakerException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * The JDBC specification wants you to use X/Open standard error codes as the SQLState parameter for an SQLException.
  * These are the standard codes for error conditions that are relevant to FileMaker.
  * These are taken from ftp://ftp.software.ibm.com/ps/products/db2/info/vr6/htm/db2m0/db2state.htm#HDRSTTMSG
  */
 public class ErrorCodes {
+	private static final Logger log = Logger.getLogger( ErrorCodes.class.getName() );
+	
+	private static final Properties errorMessages = new Properties();
+	
 	public static final String AUTH_INVALID = "28000";
+	
+	static {
+		InputStream stream = FileMakerException.class.getResourceAsStream("ErrorCodes.txt");
+		if( stream == null ) log.warning( "Couldn't locate ErrorCodes.txt file; no human-readable error messages will be generated.");
+		else try {
+			errorMessages.load(stream);
+		} catch (IOException e) {
+			log.log( Level.SEVERE, "Could not load error messages resource ErrorCodes.txt", e );
+		} finally {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	public static String getMessage( Integer errorCode ) {
+		String result = errorMessages.getProperty( String.valueOf(errorCode) );
+		if( result == null ) result = "Unknown error";
+		return result;
+	}
 }
