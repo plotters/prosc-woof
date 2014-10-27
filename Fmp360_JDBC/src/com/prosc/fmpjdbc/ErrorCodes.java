7e3f57ca2fd41dfe28723576295b30313c747856
jessebarnum <jessebarnum@5e039c8d-0506-18fe-fdc3-72db6750e6f2>
2014-10-27 20:48:33 +0000

    Added convenience methods to group error codes as temporary / validation related

--- Fmp360_JDBC/src/com/prosc/fmpjdbc/ErrorCodes.java
+++ Fmp360_JDBC/src/com/prosc/fmpjdbc/ErrorCodes.java
@@ -55,4 +55,30 @@ public class ErrorCodes {
 		if( result == null ) result = "Unknown error (" + errorCode + ")";
 		return result;
 	}
+
+	public static boolean isTemporary( int errorCode ) {
+		if( errorCode == 16 ) {
+			//16, by definition, means 'try again'
+			return true;
+		} else if( errorCode >= 200 && errorCode <= 299 ) {
+			//2xx range means permission problem. Assume that permissions will be corrected and the operation will be retried
+			return true;
+		} else if( errorCode >= 300 && errorCode <= 399 ) {
+			//Everything in 3xx range is a temporary failure due to a user interaction, such as a locked record
+			return true;
+		} else if( isValidation( errorCode ) ) {
+			//Validation failures will not succeed until we either change the validation rules or the data itself. I'm assuming that we are not changing the rules, so it's permanent until the source data changes.
+			return false;
+		} else if( errorCode >= 800 && errorCode <= 800 ) {
+			//All 8xx codes are temporary failures due to system problems, such as hard drive being full
+			return true;
+		} else {
+			//Return true for everything else
+			return true;
+		}
+	}
+	
+	public static boolean isValidation( int errorCode ) {
+		return errorCode >= 500 && errorCode <= 500;
+	}
 }
